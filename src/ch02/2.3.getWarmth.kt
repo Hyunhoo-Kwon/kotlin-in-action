a85d4c525661015d74ed360e005925cf6e5a0bbf
elon.kwon <kwongusgn@gmail.com>
2020-05-21 11:37:23 +0900

    update example

--- src/ch02/2.3.getWarmth.kt
+++ src/ch02/2.3.getWarmth.kt
@@ -2,12 +2,42 @@ package ch02
 
 import ch02.Color.*
 
+// 코틀린의 when은 자바의 switch와 비슷하다
 fun getWarmth(color: Color) = when(color) {
     RED, ORANGE, YELLOW -> "warm"
     GREEN -> "neutral"
     BLUE, INDIGO, VIOLET -> "cold"
 }
 
+// when의 분기 조건은 임의의 객체를 허용한다
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)) {
+        setOf(RED, YELLOW) -> ORANGE
+        setOf(YELLOW, BLUE) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Dirty color")
+    }
+
+// 인자가 없는 when 식을 사용하려면 각 분기의 조건이 boolean 결과를 계산하는 식이어야 한다
+fun mixOptimized(c1: Color, c2: Color) =
+    when {
+        (c1 == RED && c2 == YELLOW) ||
+                (c1 == YELLOW && c2 == RED) ->
+            ORANGE
+
+        (c1 == YELLOW && c2 == BLUE) ||
+                (c1 == BLUE && c2 == YELLOW) ->
+            GREEN
+
+        (c1 == BLUE && c2 == VIOLET) ||
+                (c1 == VIOLET && c2 == BLUE) ->
+            INDIGO
+
+        else -> throw Exception("Dirty color")
+    }
+
 fun main(args: Array<String>) {
     println(getWarmth(Color.ORANGE))
+    println(mix(YELLOW, RED))
+    println(mix(YELLOW, BLUE))
 }
