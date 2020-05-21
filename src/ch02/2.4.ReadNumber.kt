25677008aa4ef83e9c8e0461a1054700363b3366
elon.kwon <kwongusgn@gmail.com>
2020-05-21 10:27:01 +0900

    2.4. try example

---
+++ src/ch02/2.4.ReadNumber.kt
@@ -0,0 +1,21 @@
+package ch02
+
+import java.io.BufferedReader
+import java.io.StringReader
+
+fun readNumber(reader: BufferedReader) {
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e: NumberFormatException) {
+        null
+    } finally {
+        reader.close()
+    }
+
+    println(number)
+}
+
+fun main(args: Array<String>) {
+    val reader = BufferedReader(StringReader("not a number"))
+    readNumber(reader)
+}
