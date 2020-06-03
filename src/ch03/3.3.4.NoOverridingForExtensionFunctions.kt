958625d9b0de26007d24758f80c22ed1f4a10d4f
elon.kwon <kwongusgn@gmail.com>
2020-06-03 20:52:01 +0900

    Ch 03 example

--- src/ch03/3.3.4.NoOverridingForExtensionFunctions.kt
+++ src/ch03/3.3.4.NoOverridingForExtensionFunctions.kt
@@ -13,7 +13,7 @@ fun View.showOff() = println("I'm a view!")
 fun Button.showOff() = println("I'm a button!")
 
 fun main(args: Array<String>) {
-    val view: View = Button()
+    val view: View = Button()   // Button이 View의 하위 타입이기 때문에 View 타입 변수를 선언해도 Button 타입 변수를 대입할 수 있다
     view.click()
     view.showOff()  // 확장 함수는 정적으로 결정된다
 }
--- src/ch03/3.3.JoinToStringFinal.kt
+++ src/ch03/3.3.JoinToStringFinal.kt
@@ -1,6 +1,7 @@
 package ch03
 
 // 컬렉션에 대한 확장을 만든다
+// 내부적으로 확장 함수는 수신 객체를 첫 번째 인자로 받는 정적 메소드다
 fun <T> Collection<T>.joinToString(
     separator: String = ", ",
     prefix: String = "",
